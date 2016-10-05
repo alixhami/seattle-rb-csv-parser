8a12c61ad2c1f50ec0d0866b6e3c49a9207a644c
Alix Hamilton <alix.hamilton@gmail.com>
2016-10-04 20:17:39 -0700

    completed number 3

--- csv-parser.rb
+++ csv-parser.rb
@@ -1,9 +1,28 @@
 require "pp"
 
 
-def parse(filename,delimiter)
+def parse(filename,delimiter=",",quote="\"")
   csv_data = File.read(filename)
-  csv_data.split("\n").map {|line| line.split(delimiter)}
+  lines = csv_data.split("\n")
+  results = []
+  lines.each do |line|
+    row = []
+    field = ""
+    ignore_commas = false
+    line.each_char do |char|
+      if char == delimiter && !ignore_commas
+        row << field
+        field = ""
+      elsif char == quote
+        ignore_commas = !ignore_commas
+      else
+        field << char
+      end
+    end
+    row << field
+    results << row
+  end
+  results
 end
 
-pp parse("dogs.csv",",")
+pp parse("cat_breeds.csv",",")
